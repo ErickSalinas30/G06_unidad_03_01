c725711db45b2186262ce76b61daefe7682419f4
usuario <usuario@172.16.221.176>
2022-11-16 12:47:18 -0500

    Panel Colores

---
+++ src/main/java/poo_unidad_03_01/vista/Principal.java
@@ -0,0 +1,46 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+
+package poo_unidad_03_01.vista;
+
+import javax.swing.JFrame;
+
+/**
+ *
+ * @author usuario
+ */
+public class Principal {
+
+    public static void main(String[] args) {
+        
+        var v1= new Ventana0301a("Ecuador");
+        v1.iniciarComponentes();
+        v1.setVisible(true);
+        
+        /*
+        var v2= new Ventana0301a("Qatar");
+        v2.iniciarComponentes();
+        v2.setVisible(true);
+        var v3= new Ventana0301a("paises Bajos");
+        v3.iniciarComponentes();
+        v3.setVisible(true);
+        var v4= new Ventana0301a("Senegal");
+        v4.iniciarComponentes();
+        v4.setVisible(true);
+        var v5= new Ventana0301a("Argentina");
+        v5.iniciarComponentes();
+        v5.setVisible(true);
+        
+        */
+        
+        
+        
+        
+        
+        
+    }
+        
+    
+}
